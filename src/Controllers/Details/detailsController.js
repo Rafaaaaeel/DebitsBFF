51ea795a5d144ac8b7f8e58384f6621fed28e58f
Rafael Oliveira Pires <rafaelopires@icloud.com>
2023-08-02 04:08:42 -0300

    Saving files

---
+++ src/Controllers/Details/detailsController.js
@@ -0,0 +1,21 @@
+const model = require('../../Models/Details/Details')
+
+class DetailConstroller {
+
+    async all(request, response) {
+
+    }
+
+    async update(request, response) {
+
+    } 
+
+    async create(request, response) {
+
+    }
+
+    async get(request, response) {
+        
+    }
+
+}
